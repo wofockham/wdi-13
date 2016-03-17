47d6937d0593176c475598424587d11535f9c280
William of Ockham <certainthings@gmail.com>
2016-03-17 15:29:06 +1100

    CRUD CRUD CRUD CRUD

--- 07-databases/butterflies/main.rb
+++ 07-databases/butterflies/main.rb
@@ -27,6 +27,22 @@ get '/butterflies/:id' do
   erb :butterflies_show
 end
 
+get '/butterflies/:id/delete' do
+  query_db "DELETE FROM butterflies WHERE id = #{ params[:id] }"
+  redirect '/butterflies'
+end
+
+get '/butterflies/:id/edit' do
+  @butterfly = query_db "SELECT * FROM butterflies WHERE id = #{ params[:id] }"
+  erb :butterflies_edit
+end
+
+post '/butterflies/:id' do
+  query = "UPDATE butterflies SET name='#{params[:name]}', family='#{params[:family]}', image='#{params[:image]}' WHERE id=#{params[:id]}"
+  query_db query
+  redirect "/butterflies/#{ params[:id] }"
+end
+
 def query_db(sql)
   db = SQLite3::Database.new 'database.db'
   db.results_as_hash = true
