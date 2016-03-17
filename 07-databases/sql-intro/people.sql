59e9b4e46befde720364e29a6ed16b3990daf092
William of Ockham <certainthings@gmail.com>
2016-03-17 15:29:27 +1100

    SQL intro

---
+++ 07-databases/sql-intro/people.sql
@@ -0,0 +1,6 @@
+CREATE TABLE people (
+  id INTEGER PRIMARY KEY,
+  first_name TEXT,
+  last_name TEXT,
+  age INTEGER
+);
---
+++ 07-databases/sql-intro/pets.sql
@@ -0,0 +1,7 @@
+CREATE TABLE pets (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  breed TEXT,
+  age INTEGER,
+  dead INTEGER
+);
