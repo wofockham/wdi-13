5ed61998a4a399eb7537436af4f11a709297e37d
William of Ockham <certainthings@gmail.com>
2016-03-17 14:46:43 +1100

    butterfly creation

--- 07-databases/butterflies/main.rb
+++ 07-databases/butterflies/main.rb
@@ -8,9 +8,32 @@ end
 
 # INDEX
 get '/butterflies' do
+  @butterflies = query_db 'SELECT * FROM butterflies'
+  erb :butterflies_index
+end
+
+get '/butterflies/new' do
+  erb :butterflies_new
+end
+
+post '/butterflies' do
+  query = "INSERT INTO butterflies (name, family, image) VALUES ('#{params[:name]}', '#{params[:family]}', '#{params[:image]}')"
+  query_db query
+  redirect '/butterflies'
+end
+
+get '/butterflies/:id' do
+  @butterfly = query_db "SELECT * FROM butterflies WHERE id = #{ params[:id] }"
+  erb :butterflies_show
+end
+
+def query_db(sql)
   db = SQLite3::Database.new 'database.db'
   db.results_as_hash = true
 
-  @butterflies = db.execute 'SELECT * FROM butterflies'
-  erb :butterflies_index
+  puts sql
+
+  result = db.execute sql
+  db.close
+  result
 end
