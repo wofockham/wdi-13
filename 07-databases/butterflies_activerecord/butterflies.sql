e9065ff899ef16d9275c5ccf435a062197f2622b
William of Ockham <certainthings@gmail.com>
2016-03-18 13:10:53 +1100

    active record butterflies

---
+++ 07-databases/butterflies_activerecord/butterflies.sql
@@ -0,0 +1,8 @@
+CREATE TABLE butterflies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  family TEXT,
+  image TEXT
+);
+
+INSERT INTO butterflies (name, family) VALUES ('Monarch', 'Nymphalidae');
---
+++ 07-databases/butterflies_activerecord/main.rb
@@ -0,0 +1,63 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sqlite3'
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(
+  :adapter => 'sqlite3',
+  :database => 'database.db'
+)
+
+ActiveRecord::Base.logger = Logger.new(STDERR)
+
+class Butterfly < ActiveRecord::Base
+end
+
+get '/' do
+  erb :home
+end
+
+# INDEX
+get '/butterflies' do
+  @butterflies = Butterfly.all
+  erb :butterflies_index
+end
+
+get '/butterflies/new' do
+  erb :butterflies_new
+end
+
+post '/butterflies' do
+  # Butterfly.create :name => params[:name], :family => params[:family], :image => params[:image]
+  butterfly = Butterfly.new
+  butterfly.name = params[:name]
+  butterfly.family = params[:family]
+  butterfly.image = params[:image]
+  butterfly.save
+  redirect '/butterflies'
+end
+
+get '/butterflies/:id' do
+  @butterfly = Butterfly.find params[:id]
+  erb :butterflies_show
+end
+
+get '/butterflies/:id/delete' do
+  butterfly = Butterfly.find params[:id]
+  butterfly.destroy
+  redirect '/butterflies'
+end
+
+get '/butterflies/:id/edit' do
+  @butterfly = Butterfly.find params[:id]
+  erb :butterflies_edit
+end
+
+post '/butterflies/:id' do
+  butterfly = Butterfly.find params[:id]
+  butterfly.name = params[:name]
+  butterfly.family = params[:family]
+  butterfly.image = params[:image]
+  butterfly.save
+  redirect "/butterflies/#{ params[:id] }"
+end
