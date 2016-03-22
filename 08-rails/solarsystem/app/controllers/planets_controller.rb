2a5cd6dd8c973746184f3b0676bf852f355ddd1b
William of Ockham <certainthings@gmail.com>
2016-03-22 15:28:49 +1100

    rails planet crud app

--- 08-rails/solarsystem/app/controllers/planets_controller.rb
+++ 08-rails/solarsystem/app/controllers/planets_controller.rb
@@ -15,6 +15,22 @@ class PlanetsController < ApplicationController
     redirect_to planet_path(planet.id)
   end
 
+  def destroy
+    planet = Planet.find params[:id]
+    planet.destroy
+    redirect_to planets_path
+  end
+
+  def edit
+    @planet = Planet.find params[:id]
+  end
+
+  def update
+    planet = Planet.find params[:id]
+    planet.update planet_params
+    redirect_to planet_path(planet.id)
+  end
+
   private
   def planet_params
     # Safe whitelist of the specific params we're permitting
--- 08-rails/solarsystem/config/routes.rb
+++ 08-rails/solarsystem/config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   get '/planets/:id' => 'planets#show', :as => 'planet'
   post '/planets' => 'planets#create'
   post '/planets/:id/delete' => 'planets#destroy', :as => 'planet_delete'
+  get '/planets/:id/edit' => 'planets#edit', :as => 'planet_edit'
+  post '/planets/:id' => 'planets#update'
 end
