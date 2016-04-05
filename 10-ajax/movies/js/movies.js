43cc324c8011a7c391ea9dd33b8489b2d295b16c
William of Ockham <certainthings@gmail.com>
2016-04-05 10:41:57 +1000

    sample movie solution form

---
+++ 10-ajax/movies/js/movies.js
@@ -0,0 +1,7 @@
+
+window.addEventListener('load', function () {
+  var button = document.getElementById('search');
+  button.addEventListener('click', function () {
+    console.log('you clicked');
+  });
+});
