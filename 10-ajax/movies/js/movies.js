ac813623f426192fb0dd1264f53def56ff3e264c
William of Ockham <certainthings@gmail.com>
2016-04-05 11:25:33 +1000

    vanilla AJAX movies

--- 10-ajax/movies/js/movies.js
+++ 10-ajax/movies/js/movies.js
@@ -1,7 +1,30 @@
-
+// Similar to (but not as good as) $(document).ready();
 window.addEventListener('load', function () {
-  var button = document.getElementById('search');
-  button.addEventListener('click', function () {
-    console.log('you clicked');
+
+  var form = document.getElementById('movie-lookup');
+  form.addEventListener('submit', function (event) {
+
+    event.preventDefault(); // Don't let the browser submit the form for us.
+
+    var movieTitle = document.getElementById('movie-title').value;
+    var movieURL = 'http://omdbapi.com/?t=' + movieTitle;
+
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+      if (request.readyState !== 4) { return };
+
+      var movieInfo = JSON.parse(request.responseText);
+
+      var moviePoster = movieInfo.Poster;
+      var image = document.createElement('img');
+      image.src = moviePoster;
+      document.getElementById('result').innerHTML = ''; // Clear the result
+      document.getElementById('result').appendChild(image);
+
+    };
+    request.open('GET', movieURL);
+    request.send();
+
   });
+
 });
