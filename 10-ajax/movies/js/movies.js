8f1bd225ff28c98eba568f065563bb3d43bbf849
William of Ockham <certainthings@gmail.com>
2016-04-05 11:53:12 +1000

    jquerified movies

--- 10-ajax/movies/js/movies.js
+++ 10-ajax/movies/js/movies.js
@@ -1,30 +1,18 @@
-// Similar to (but not as good as) $(document).ready();
-window.addEventListener('load', function () {
+$(document).ready(function () {
 
-  var form = document.getElementById('movie-lookup');
-  form.addEventListener('submit', function (event) {
+  $('#movie-lookup').on('submit', function (event) {
 
     event.preventDefault(); // Don't let the browser submit the form for us.
 
-    var movieTitle = document.getElementById('movie-title').value;
+    var movieTitle = $('#movie-title').val();
     var movieURL = 'http://omdbapi.com/?t=' + movieTitle;
 
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function () {
-      if (request.readyState !== 4) { return };
-
-      var movieInfo = JSON.parse(request.responseText);
-
-      var moviePoster = movieInfo.Poster;
-      var image = document.createElement('img');
-      image.src = moviePoster;
-      document.getElementById('result').innerHTML = ''; // Clear the result
-      document.getElementById('result').appendChild(image);
-
-    };
-    request.open('GET', movieURL);
-    request.send();
-
+    $.ajax(movieURL, {
+      success: function (data) {
+        var $image = $('<img>', {src: data.Poster});
+        $('#result').html($image);
+      }
+    });
   });
 
 });
