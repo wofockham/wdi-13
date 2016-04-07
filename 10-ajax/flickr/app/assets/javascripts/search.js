e9c6950f9bd82c6c63b2e0976058adf11cf3e22f
William of Ockham <certainthings@gmail.com>
2016-04-07 11:33:48 +1000

    debounced flickr

--- 10-ajax/flickr/app/assets/javascripts/search.js
+++ 10-ajax/flickr/app/assets/javascripts/search.js
@@ -1,20 +1,34 @@
 $(document).ready(function () {
 
+  var currentPage = 1;
+  var endOfResults = false;
+  var query;
+
   var searchFlickr = function (term) {
+    if (endOfResults) {
+      return;
+    }
+
     var flickrURL = 'https://api.flickr.com/services/rest/?jsoncallback=?';
     $.getJSON(flickrURL, {
       // You can find additional params to add here in the docs
       method: 'flickr.photos.search',
       api_key: '2f5ac274ecfac5a455f38745704ad084',
       text: term,
+      page: currentPage++,
       format: 'json'
     }).done(function (data) {
+      if (data.photos.page === data.photos.pages) {
+        endOfResults = true; // We've reached the last page.
+      }
       for (var i = 0; i < data.photos.photo.length; i++) {
         var url = generateURL(data.photos.photo[i]);
         displayPhoto(url);
       }
     });
   };
+
+  var debouncedSearchFlickr = _.debounce(searchFlickr, 1000, true);
 
   var displayPhoto = function (url) {
     var $img = $('<img>', {src: url});
@@ -38,8 +52,10 @@ $(document).ready(function () {
   $('#search').on('submit', function (e) {
     e.preventDefault();
 
-    var query = $('#query').val();
     $('#results').empty();
+    currentPage = 1;
+    endOfResults = false;
+    query = $('#query').val();
     searchFlickr(query);
 
   });
@@ -52,7 +68,7 @@ $(document).ready(function () {
     var fetchThreshold = 500;
 
     if (pixelsFromBottom < fetchThreshold) {
-      console.log('near the bottom, HINT: SEARCH FLICKR MORE HERE');
+      debouncedSearchFlickr(query);
     }
 
   });
