1ebda9da368ad6fde0b1ffee9d374b2a1a7405cd
William of Ockham <certainthings@gmail.com>
2016-04-08 14:54:02 +1000

    backbone hell

--- 11-backbone/backbone-blog/js/main.js
+++ 11-backbone/backbone-blog/js/main.js
@@ -25,12 +25,58 @@ var posts = new Posts([
   })
 ]);
 
+var AppView = Backbone.View.extend({
+  el: '#main',
+  render: function () {
+    var template = $('#appViewTemplate').html();
+    this.$el.html(template);
+    this.collection.each(function (p) {
+      var postListView = new PostListView({model: p});
+      postListView.render();
+    });
+  }
+});
+
+var PostListView = Backbone.View.extend({
+  tagName: 'li',
+  events: {
+    'click': 'showPost'
+  },
+  render: function () {
+    this.$el.text( this.model.get('title') );
+    this.$el.appendTo('#posts');
+  },
+  showPost: function () {
+    router.navigate('posts/' + this.model.get('id'), true);
+  }
+});
+
+var PostView = Backbone.View.extend({
+  el: '#main',
+  render: function () {
+    var $article = $('<article></article>');
+
+    $article.append('<h1>' + this.model.get('title') + '</h1>');
+    $article.append('<h2>' + 'By: ' + this.model.get('author') + '</h2>');
+    $article.append('<p>' + this.model.get('content') + '</p>');
+
+    this.$el.html( $article );
+  }
+});
+
 var Router = Backbone.Router.extend({
   routes: {
-    '': 'index'
+    '': 'index',
+    'posts/:id': 'show'
   },
   index: function () {
-    console.log('Index page coming soon');
+    var appView = new AppView({collection: posts});
+    appView.render();
+  },
+  show: function (id) {
+    var post = posts.get(id);
+    var postView = new PostView({model: post});
+    postView.render();
   }
 });
 
