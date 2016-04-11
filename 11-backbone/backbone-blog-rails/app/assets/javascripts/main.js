6f03501a0f800e092d8756f49fd4ad53a71c431c
William of Ockham <certainthings@gmail.com>
2016-04-11 12:33:32 +1000

    Only start Backbone on pages where #main is present

--- 11-backbone/backbone-blog-rails/app/assets/javascripts/main.js
+++ 11-backbone/backbone-blog-rails/app/assets/javascripts/main.js
@@ -7,6 +7,12 @@ var posts = new app.Posts();
 var router = new app.Router();
 
 $(document).ready(function () {
+
+  // Don't start Backbone on non-Backbone pages
+  if ($('#main').length === 0) {
+    return;
+  }
+
   posts.fetch().done(function () {
     Backbone.history.start(); // Starts the router and pays attention to changes in the URL.
   });
