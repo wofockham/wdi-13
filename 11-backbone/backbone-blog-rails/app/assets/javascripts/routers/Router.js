5ee6bf58012cc9b72266e009d038f5ee56f19a80
William of Ockham <certainthings@gmail.com>
2016-04-11 13:58:52 +1000

    Bonus: 404-style handling for Backbone

--- 11-backbone/backbone-blog-rails/app/assets/javascripts/routers/Router.js
+++ 11-backbone/backbone-blog-rails/app/assets/javascripts/routers/Router.js
@@ -4,7 +4,8 @@ var app = app || {};
 app.Router = Backbone.Router.extend({
   routes: {
     '': 'index',
-    'posts/:id': 'show'
+    'posts/:id': 'show',
+    ':default': 'goHome'
   },
   index: function () {
     var appView = new app.AppView({collection: posts});
@@ -12,7 +13,13 @@ app.Router = Backbone.Router.extend({
   },
   show: function (id) {
     var post = posts.get(id);
+    if (! post) {
+      return router.navigate('', true);
+    }
     var postView = new app.PostView({model: post});
     postView.render();
+  },
+  goHome: function () {
+    router.navigate('', true);
   }
 });
