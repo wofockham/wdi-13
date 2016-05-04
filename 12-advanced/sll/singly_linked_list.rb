48236df244596e2f1e93cbf070eef4d74e8f49a1
William of Ockham <certainthings@gmail.com>
2016-05-04 11:30:23 +1000

    Singly Linked List probably has bugs

--- 12-advanced/sll/singly_linked_list.rb
+++ 12-advanced/sll/singly_linked_list.rb
@@ -1,8 +1,11 @@
 class SinglyLinkedList
+
+  include Enumerable # Mixin
+
   attr_accessor :head
 
   def initialize(value=nil)
-    @head = Node.new(value)
+    @head = Node.new(value) if value
   end
 
   def last
@@ -24,12 +27,24 @@ class SinglyLinkedList
   end
 
   def insert_after(node, new_node)
+    new_node.next = node.next
+    node.next = new_node
   end
 
   def shift # Acts like some_array.shift
+    node = @head
+    @head = node.next
+    node
   end
 
   def length
+    tally = 0
+    node = @head
+    while node.respond_to? :next
+      node = node.next
+      tally += 1
+    end
+    tally
   end
 
   def find(needle)
@@ -37,25 +52,25 @@ class SinglyLinkedList
   end
 
   def reverse
+    reverse_list = SinglyLinkedList.new
+    node = @head
+    while node
+      reverse_list.prepend(node.value)
+      node = node.next
+    end
+    reverse_list
   end
 
   def reverse!
+    @head = reverse.head
   end
 
   def each
-  end
-
-  # Bonus
-  def map
-  end
-
-  def inject
-  end
-
-  def any?
-  end
-
-  def all?
+    node = @head
+    while node
+      yield node.value if block_given?
+      node = node.next
+    end
   end
 
   # Look at replacing this with a Ruby Struct
